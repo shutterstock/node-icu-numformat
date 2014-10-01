1f1baf3d1f8a68a77fcecc583c03c87abd429cde
Brian Phillips <bphillips@shutterstock.com>
2014-10-01 11:21:16 -0500

    docs + less brittle tests

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Summary
 
-This node.js module provides bindings to the libicu number formatting functions.
+This module provides bindings to the [libicu](http://site.icu-project.org/) [number formatting](http://userguide.icu-project.org/formatparse/numbers) functions.
 
-# Example
+## Example
 
 ```Javascript
 var Formatter = require('icu-numformat');
+
 var en = new Formatter('en_US');
 console.log(en.formatDecimal(1234.56)); // 1,234.56
 
@@ -15,3 +16,119 @@ console.log(fr.formatCurrency(1234.56, 'EUR')); // 1 234,56 €
 var preciseEn = new Formatter('en_US', { max_fraction_digits: 4 });
 console.log(preciseEn.formatDecimal(1234.5678)); // 1,234.5678
 ```
+
+## Installation
+
+**NOTE:** This module relies on the `icu-config` tool being installed somewhere in your `PATH`. This typically comes in a package like `libicu-dev` (ubuntu/debian) or `icu4c` (brew). You can also [download](http://site.icu-project.org/download) and [install](http://userguide.icu-project.org/icufaq) the package from source.
+
+Assuming you've completed the above, you may then install the node module with npm:
+
+```
+$ npm install icu-numformat
+```
+
+## Creating a Formatter Object
+
+The constructor requires a single argument indicating the locale to use when formatting the numbers:
+
+```
+var formatter = new Formatter('fr_FR');
+```
+
+You may also specify a second argument of options that will affect how numbers are formatted.  For instance:
+
+```
+var formatter = new Formatter('da_DK', { max_fraction_digits: 4} );
+```
+
+The supported [formatting attributes](http://icu-project.org/apiref/icu4c/unum_8h.html#a22c3085f2e722f578a92c15a3346097f) can be introspected as follows:
+
+```
+console.log("Supported Attributes: ", Formatter.attributeNames);
+```
+
+## Formatting Values
+
+Once you have a formatter object, you can re-use it to format numbers
+appropriately for the locale of that formatter. Each of the following
+formatter methods listed below returns a string.
+
+#### Decimals
+
+```
+formatter.formatDecimal(123456.789);
+```
+
+#### Currencies
+
+```
+formatter.formatCurrency(1234.56, 'EUR'); // fr_FR: 1 234,56 €
+```
+
+Or, using the ISO currency code (only supported in libicu >=53):
+
+```
+formatter.formatCurrency(1234.56, 'EUR', 'ISO'); // fr_FR: 1 234,56 EUR
+```
+
+Or finally, using the plural form of the currency name (only supported in libicu >=53):
+
+```
+formatter.formatCurrency(1234.56, 'EUR', 'plural'); // fr_FR: 1 234,56 euros
+```
+
+#### Percents
+
+```
+formatter.formatPercent(0.23); // en_US: 23%
+```
+
+#### Words
+
+```
+formatter.formatAsWords(1000); // en_US: one thousand
+```
+
+#### Ordinals
+
+```
+formatter.formatAsOrdinal(21); // en_US: 21st
+```
+
+#### Scientific Notation
+
+```
+formatter.formatScientific(1234.56); // en_US: 1.23456E3
+```
+
+#### Duration
+
+```
+formatter.formatAsDuration(300); // en_US: 5:00
+```
+
+# Author
+
+This library was written by Brian Phillips at [Shutterstock](http://www.shutterstock.com)
+
+## License
+
+Copyright (C) 2014 by Shutterstock Images, LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
--- test/test.js
+++ test/test.js
@@ -2,57 +2,63 @@ var expect = require('chai').expect;
 var Formatter = require('../lib/index.js');
 console.log("# ICU Version: " + Formatter.icuVersion);
 
-var localeFormats = {
-	en_US: [
-		[1, '1'],
-		[1234.56, '1,234.56'],
-		[1234.56789, '1,234.568'],
-	],
-	fr_FR: [
-		[1, '1'],
-		[1234.56, '1\xA0234,56']
-	],
-	de_CH: [
-		[1, '1'],
-		[1234.56, '1\'234.56'],
-	],
-	hi_IN: [
-		[1, '१'],
-		[1000000, '१०,००,०००']
-	]
-};
-describe('NumberFormatter', function() {
-	it('should format per locale', function() {
-		Object.keys(localeFormats).forEach(function(locale) {
-			var formatter = new Formatter(locale);
-			localeFormats[locale].forEach(function(pair) {
-				expect(formatter.formatDecimal(pair[0])).to.equal(pair[1]);
-			});
-		});
-	});
-});
-
 describe('Formatter', function() {
 	var locale = 'en_US';
 	var formatter = new Formatter(locale);
 
-	it('should allow us to spell en_US numbers', function() {
-		expect(formatter.formatAsWords(1000)).to.equal('one thousand');
-	});
+  describe('formatter methods', function() {
+    it('should allow us to format decimal values', function() {
+      expect(formatter.formatDecimal(1234.56)).to.equal('1,234.56');
+    });
 
-	it('should allow us to get ordinal numbers', function() {
-		// not sure why but sometimes this comes out as ascii and sometimes utf-8
-		expect(formatter.formatAsOrdinal(1000)).to.match(/^1,000(th|ᵗʰ)$/);
-	});
+    it('should format currency values', function() {
+      expect(formatter.formatCurrency(1234.56, 'USD')).to.equal('$1,234.56');
+    });
 
-	it('should allow us to format duration values', function() {
-		expect(formatter.format('duration', 60 * 60 + (23 * 60) + 45)).to.equal('1:23:45');
-	});
+    describe('version >= 53 features', function() {
+      if(Formatter.icuVersion >= 53) {
+        it('should allow for formatting with the ISO currency code', function() {
+          expect(formatter.formatCurrency(1234.56, 'USD', 'ISO')).to.be.equal('USD1,234.56');
+        });
+
+        it('should format currency values with the plural form', function() {
+          expect(formatter.formatCurrency(1234.56, 'USD', 'plural')).to.equal('1,234.56 US Dollars');
+        });
+      } else {
+        it('should throw an exception when using these features without the right lib version', function() {
+          expect(function() { formatter.formatCurrency(1234.56, 'USD', 'ISO'); }).to.throw(Error);
+        });
+      }
+    });
+
+    it('should allow us to format percent values', function() {
+      expect(formatter.formatPercent(0.24)).to.equal('24%');
+      expect(formatter.formatPercent(0.01)).to.equal('1%');
+    });
+
+    it('should allow us to spell en_US numbers', function() {
+      expect(formatter.formatAsWords(1000)).to.equal('one thousand');
+    });
+
+    it('should allow us to get ordinal numbers', function() {
+      // not sure why but sometimes this comes out as ascii and sometimes utf-8
+      expect(formatter.formatAsOrdinal(1000)).to.match(/^1,000(th|ᵗʰ)$/);
+    });
+
+    it('should allow us to format duration values', function() {
+      expect(formatter.formatAsDuration(60 * 60 + (23 * 60) + 45)).to.equal('1:23:45');
+    });
+
+    it('should allow us to format values using scientific notation', function() {
+      expect(formatter.formatScientific(1234.56)).to.equal('1.23456E3');
+    });
+  });
 
 	describe('attributes', function() {
 		it('should allow us to set attributes', function() {
 			var formatter = new Formatter(locale, {max_fraction_digits: 4});
 			expect(formatter.formatDecimal(123.1294)).to.equal('123.1294');
+
 			formatter.setAttributes({max_fraction_digits: 2});
 			expect(formatter.formatDecimal(123.1294)).to.equal('123.13');
 		});
@@ -61,6 +67,7 @@ describe('Formatter', function() {
 			var formatter = new Formatter(locale, {max_fraction_digits: 3});
 			expect(formatter.formatDecimal(123.1295)).to.equal('123.13');
 			expect(formatter.formatPercent(0.00001)).to.equal('0.001%');
+
 			formatter.setAttributes('percent', {max_fraction_digits: 2});
 			expect(formatter.formatPercent(0.00001)).to.equal('0%');
 		});
@@ -69,7 +76,7 @@ describe('Formatter', function() {
 	describe('errors', function() {
 		var formatter = new Formatter('en');
 		it('should throw exceptions', function() {
-			expect(function() { formatter.format('decimal'); }).to.throw(TypeError, /Argument 1 must be a number/);
+			expect(function() { formatter.formatDecimal(); }).to.throw(TypeError, /Argument 1 must be a number/);
 		});
 	});
 });
